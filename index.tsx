ad3277a886a67da99272f740bba69067b571fd23
DELL <dell@broadway.local>
2026-01-30 20:23:04 +0300

    fix(core): force unregister service workers to clear cache

--- index.tsx
+++ index.tsx
@@ -7,6 +7,16 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
+// Force unregister service workers to prevent caching of old versions
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    for (let registration of registrations) {
+      console.log('Unregistering Service Worker:', registration);
+      registration.unregister();
+    }
+  });
+}
+
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
